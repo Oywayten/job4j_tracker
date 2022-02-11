8d8f5bfbba9e990e7800993ae1f95c32b1bca21f
Vitaliy123 <cpc1251@mail.ru>
2022-02-11 21:15:53 +0300

    3.2. Зачем нужно приведение типов [#310019]

--- src/main/java/ru/job4j/oop/HierarchyUsage.java
+++ src/main/java/ru/job4j/oop/HierarchyUsage.java
@@ -2,14 +2,12 @@ package ru.job4j.oop;
 
 public class HierarchyUsage {
     public static void main(String[] args) {
-        Car car = new Car();
-        Transport tp = car;
-        Object obj = car;
-        Object ocar = new Car();
-        Transport carFromObject = (Transport) car;
-        Object bicycle = new Bicycle();
-        System.out.println(new Car());
-        System.out.println(new Bicycle());
-        System.out.println(new Object());
+        Vehicle train = new Train();
+        Vehicle plain = new Plain();
+        Vehicle bus = new Bus();
+        Vehicle[] vehicles = new Vehicle[]{train, plain, bus};
+        for (Vehicle veh : vehicles) {
+            veh.move();
+        }
     }
 }
