2eb1f435f53a7c1b8c048ad33a826a08e365dd88
Vitaliy123 <cpc1251@mail.ru>
2022-08-10 23:00:55 +0300

    job4j-35 4. Принципы раннего возврата и охранных выражений [#504875]
    Написал методы, задокументировал, сделал юнит-тесты.
    Методы писал с учетом принципов солид.

---
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -0,0 +1,65 @@
+package ru.job4j.early;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @Test
+    public void whenisValid() {
+        String password = "#aLOVbj31~";
+        String actual = PasswordValidator.validate(password);
+        String expected = PasswordValidator.ALL_RIGHT;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenLengthIsShort() {
+        String password = "aLOVbj~";
+        String actual = PasswordValidator.validate(password);
+        String expected = PasswordValidator.LENGTH_ERROR;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenWithoutUpperCase() {
+        String password = "#alowbj31~";
+        String actual = PasswordValidator.validate(password);
+        String expected = PasswordValidator.UP_ERROR;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenWithoutLowerCase() {
+        String password = "#ALOWBJ31~";
+        String actual = PasswordValidator.validate(password);
+        String expected = PasswordValidator.LOW_ERROR;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenWithoutNums() {
+        String password = "#aLOVbj~";
+        String actual = PasswordValidator.validate(password);
+        String expected = PasswordValidator.DIGIT_ERROR;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenWithoutChars() {
+        String password = "fdaLowj31";
+        String actual = PasswordValidator.validate(password);
+        String expected = PasswordValidator.CHAR_ERROR;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenWithQwErtY() {
+        String password = "#ALOWQwErtYBJ31~";
+        String actual = PasswordValidator.validate(password);
+        String expected = PasswordValidator.QWERTY_ERROR;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
