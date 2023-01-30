ee67d969ca18f22c600d76bc50bf949d495b8056
oywayten <cpc1251@mail.ru>
2023-01-30 14:41:40 +0300

    Fixe the test and add dependency

--- src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -1,13 +1,11 @@
 package ru.job4j.tracker;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ItemDescByNameTest {
 
@@ -16,6 +14,6 @@ public class ItemDescByNameTest {
         List<Item> items = Arrays.asList(new Item("First"), new Item("Third"), new Item("Second"));
         items.sort(new ItemDescByName().reversed());
         List<Item> expected = Arrays.asList(new Item("Third"), new Item("Second"), new Item("First"));
-        Assert.assertEquals(items, expected);
+        assertThat(items).isEqualTo(expected);
     }
 }
