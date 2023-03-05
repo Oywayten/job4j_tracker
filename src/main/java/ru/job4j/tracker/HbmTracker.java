11e93203477a445d44c74e1e2a2c25b353052b00
oywayten <cpc1251@mail.ru>
2023-03-05 23:23:00 +0300

    HbmTracker [#287705]

---
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -0,0 +1,126 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class HbmTracker implements Store, AutoCloseable {
+
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+    private final SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+    @Override
+    public Item add(Item item) {
+        final Session session = sf.openSession();
+        int id;
+        try {
+            session.beginTransaction();
+            id = (Integer) session.save(item);
+            item.setId(id);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        final Session session = sf.openSession();
+        boolean isReplaced = false;
+        try {
+            session.beginTransaction();
+            item.setId(id);
+            session.update(item);
+            session.getTransaction().commit();
+            isReplaced = true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return isReplaced;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        final Session session = sf.openSession();
+        boolean isDeleted = false;
+        try {
+            session.beginTransaction();
+            Item item = new Item();
+            item.setId(id);
+            session.delete(item);
+            session.getTransaction().commit();
+            isDeleted = true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return isDeleted;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        final Session session = sf.openSession();
+        List<Item> itemList = List.of();
+        try {
+            session.beginTransaction();
+            itemList = session.createQuery("from Item as i", Item.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return itemList;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        Session session = sf.openSession();
+        List<Item> itemList = List.of();
+        try {
+            session.beginTransaction();
+            itemList = session.createQuery("from Item as i where i.name = :name", Item.class)
+                    .setParameter("name", key)
+                    .getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return itemList;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Session session = sf.openSession();
+        Item item = null;
+        try {
+            session.beginTransaction();
+            item = session.createQuery("from Item as i where i.id = :id", Item.class)
+                    .setParameter("id", id)
+                    .uniqueResult();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return item;
+    }
+
+    @Override
+    public void close() {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+}
